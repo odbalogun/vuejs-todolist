c85b16e2d01197674122ef232bf2742f2faba8f3
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-10-04 20:25:37 +0100

    Fixed tabs and spaces issues

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>Todo A</li>
+      <li>Todo B</li>
+      <li>Todo C</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['todos']
+}
+</script>
+
+<style>
+
+</style>
