a20abf688693db4524bc133b75212874e48412c5
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-10-05 12:25:00 +0100

    Added delete functionality

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Completed Tasks: {{ todos.filter(todo => { return todo.done === true}).length }}</p>
     <p>Pending Tasks: {{ todos.filter(todo => { return todo.done === false}).length }}</p>
-    <todo v-for="todo in todos" v-bind:todo="todo" :key="todo"></todo>
+    <todo v-for="todo in todos" v-bind:todo="todo" :key="todo" v-on:delete-todo="deleteTodo"></todo>
   </div>
 </template>
 
@@ -13,6 +13,12 @@ export default {
   props: ['todos'],
   components: {
     Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
+    }
   }
 }
 </script>
